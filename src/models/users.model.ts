6ffd35e072e8556c71b637626d1752fa1c925dee
Eric Tsang <eric@m1.local>
2022-03-22 13:41:57 +0800

    findUserByAddress

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -2,10 +2,10 @@ import { User } from '@interfaces/users.interface';
 
 // password: password
 const userModel: User[] = [
-  { id: 1, email: 'example1@email.com', password: '$2b$10$TBEfaCe1oo.2jfkBDWcj/usBj4oECsW2wOoDXpCa2IH9xqCpEK/hC' },
-  { id: 2, email: 'example2@email.com', password: '$2b$10$TBEfaCe1oo.2jfkBDWcj/usBj4oECsW2wOoDXpCa2IH9xqCpEK/hC' },
-  { id: 3, email: 'example3@email.com', password: '$2b$10$TBEfaCe1oo.2jfkBDWcj/usBj4oECsW2wOoDXpCa2IH9xqCpEK/hC' },
-  { id: 4, email: 'example4@email.com', password: '$2b$10$TBEfaCe1oo.2jfkBDWcj/usBj4oECsW2wOoDXpCa2IH9xqCpEK/hC' },
+  { id: 1, address: '0x1' },
+  { id: 2, address: '0x2' },
+  { id: 3, address: '0x3' },
+  { id: 4, address: '0x4' },
 ];
 
 export default userModel;
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,59 +1,25 @@
-import { hash } from 'bcrypt';
-import { CreateUserDto } from '@dtos/users.dto';
+// import { hash } from 'bcrypt';
+// import { CreateUserDto } from '@dtos/users.dto';
 import { HttpException } from '@exceptions/HttpException';
 import { User } from '@interfaces/users.interface';
 import userModel from '@models/users.model';
-import { isEmpty } from '@utils/util';
+// import { isEmpty } from '@utils/util';
+
+import { ethers } from 'ethers';
 
 class UserService {
   public users = userModel;
 
-  public async findAllUser(): Promise<User[]> {
-    const users: User[] = this.users;
-    return users;
-  }
-
-  public async findUserById(userId: number): Promise<User> {
-    const findUser: User = this.users.find(user => user.id === userId);
-    if (!findUser) throw new HttpException(409, "You're not user");
+  public async findUserByAddress(address: string): Promise<User> {
+    console.log('address', address);
+    const findUser: User = {
+      id: 1,
+      address: address,
+    };
+    // const findUser: User = this.users.find(user => user.address === address);
+    // if (!findUser) throw new HttpException(404, 'Not Found');
 
     return findUser;
-  }
-
-  public async createUser(userData: CreateUserDto): Promise<User> {
-    if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
-
-    const findUser: User = this.users.find(user => user.email === userData.email);
-    if (findUser) throw new HttpException(409, `Your email ${userData.email} already exists`);
-
-    const hashedPassword = await hash(userData.password, 10);
-    const createUserData: User = { id: this.users.length + 1, ...userData, password: hashedPassword };
-    this.users = [...this.users, createUserData];
-
-    return createUserData;
-  }
-
-  public async updateUser(userId: number, userData: CreateUserDto): Promise<User[]> {
-    if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
-
-    const findUser: User = this.users.find(user => user.id === userId);
-    if (!findUser) throw new HttpException(409, "You're not user");
-
-    const hashedPassword = await hash(userData.password, 10);
-    const updateUserData: User[] = this.users.map((user: User) => {
-      if (user.id === findUser.id) user = { id: userId, ...userData, password: hashedPassword };
-      return user;
-    });
-
-    return updateUserData;
-  }
-
-  public async deleteUser(userId: number): Promise<User[]> {
-    const findUser: User = this.users.find(user => user.id === userId);
-    if (!findUser) throw new HttpException(409, "You're not user");
-
-    const deleteUserData: User[] = this.users.filter(user => user.id !== findUser.id);
-    return deleteUserData;
   }
 }
 
